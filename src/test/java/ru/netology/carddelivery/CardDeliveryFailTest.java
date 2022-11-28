026193720a118afceac5183cc0f4b1592fa1b59a
MariaG <MGrigorenko@bellintegrator.com>
2022-11-28 22:56:27 +0300

    reformat code

--- src/test/java/ru/netology/carddelivery/CardDeliveryFailTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryFailTest.java
@@ -1,6 +1,5 @@
 package ru.netology.carddelivery;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -25,7 +24,6 @@ public class CardDeliveryFailTest {
     public String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-
 
 
     @Test
--- src/test/java/ru/netology/carddelivery/CardDeliverySuccessTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliverySuccessTest.java
@@ -1,7 +1,6 @@
 package ru.netology.carddelivery;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
