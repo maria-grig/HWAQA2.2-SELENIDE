cdc8aff6318cb9e3a145a53d19fac9909eea4300
MariaG <MGrigorenko@bellintegrator.com>
2022-11-28 22:25:43 +0300

    refactor

--- src/test/java/ru/netology/carddelivery/CardDeliveryFailTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryFailTest.java
@@ -18,7 +18,6 @@ public class CardDeliveryFailTest {
     @BeforeEach
     void setUpAll() {
         Configuration.timeout = 10_000;
-        //Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
 
